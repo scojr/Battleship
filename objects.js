d33b9682e0afa233ebf8d13ecfe1b5b9ea8c53f7
scojr <180873402+scojr@users.noreply.github.com>
2025-05-19 18:52:46 -0500

    Create Ship and Gameboard classes

--- objects.js
+++ objects.js
@@ -1,13 +1,58 @@
 export class Ship {
   constructor(length) {
     this.length = length;
+    this.health = length;
   }
-  health = this.length;
   hit() {
     this.health -= 1;
   }
   isSunk() {
-    if (this.health > 1) return true;
+    if (this.health < 1) return true;
     else return false;
+  }
+}
+
+class Cell {
+  constructor(value = null) {
+    this.value = value;
+  }
+}
+
+export class Gameboard {
+  constructor() {
+    this.grid = populateGrid();
+
+    function populateGrid() {
+      const grid = []
+      for (let i = 0; i < 10; i++) {
+        const row = [];
+        for (let i = 0; i < 10; i++) {
+          row.push(new Cell(null));
+        }
+        grid.push(row);
+      }
+      return grid;
+    }
+  }
+  place() {
+
+  }
+  receiveAttack() {
+
+  }
+  shipsSunk() {
+
+  }
+  getCell(x, y) {
+    return this.grid[x][y];
+  }
+  getGrid() {
+    return this.grid;
+  }
+}
+
+export class Player {
+  constructor() {
+
   }
 }
--- objects.test.js
+++ objects.test.js
@@ -1,6 +1,37 @@
-import { Ship } from './objects';
-jest.mock('./objects');
+import { Ship, Gameboard } from './objects';
 
-beforeEach(() => {
-  Ship.mockClear();
+
+describe('Ship', () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(5);
+  });
+
+  test('new Ship isSunk() returns false', () => {
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test('after 5 hits isSunk() returns true', () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+})
+
+describe('Gameboard', () => {
+  jest.mock('./objects');
+  let gameboard;
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test('new Gameboard grid contains cells with null value', () => {
+    expect(gameboard.getCell(0, 0)).toEqual({ value: null });
+    expect(gameboard.getCell(4, 4)).toEqual({ value: null });
+    expect(gameboard.getCell(9, 9)).toEqual({ value: null });
+  });
 })
