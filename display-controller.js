a4a327175410e249e082f0e1e3b0f04daf136727
scojr <180873402+scojr@users.noreply.github.com>
2025-06-01 16:34:59 -0500

    Add temporary function to place ships

--- display-controller.js
+++ display-controller.js
@@ -1,10 +1,34 @@
-const gameboardContainer = document.querySelector('.gameboard-container')
-const playerGrid = gameboardContainer.querySelector('.player.grid');
-const opponentGrid = gameboardContainer.querySelector('.opponent.grid');
+const elements = {
+  gameboardContainer: document.querySelector('.gameboard-container'),
+  playerGrid: document.querySelector('.player.grid'),
+  opponentGrid: document.querySelector('.opponent.grid'),
+  opponentHealth: document.querySelector('.score .opponent.text'),
+  playerHealth: document.querySelector('.score .opponent.text'),
+  displayHeader: document.querySelector('.display .title'),
+  displayMessage: document.querySelector('.display .message'),
+}
+
+const scoreInterface = {
+  updatePlayerScore: function (value) {
+    elements.playerHealth.textContent = value;
+  },
+  updateOpponentScore: function (value) {
+    elements.opponentHealth.textContent = value;
+  },
+}
+
+const displayInterface = {
+  updateHeader: function (string) {
+    elements.displayHeader.textContent = string;
+  },
+  updateMessage: function (string) {
+    elements.displayHeader.textContent = string;
+  }
+}
 
 export function drawBoard() {
-  fillGrids(playerGrid, 'player');
-  fillGrids(opponentGrid, 'opponent');
+  fillGrids(elements.playerGrid, 'player');
+  fillGrids(elements.opponentGrid, 'opponent');
 }
 
 function fillGrids(element, name) {
@@ -12,27 +36,33 @@ function fillGrids(element, name) {
     let x = i;
     let y = Math.floor(i / 10);
     const cell = document.createElement('div');
-    cell.addEventListener('click', e => {
-      console.log("test");
-    })
-    cell.classList.add('cell', 'empty', name, `x${x % 10}`, `y${y}`)
+    cell.addEventListener('click', e => cellClick(e.target, name))
+    cell.classList.add('cell', name, `x${x % 10}`, `y${y}`)
     cell.dataset.x = x % 10;
     cell.dataset.y = y;
     element.append(cell);
   }
 }
 
-export function refreshPlayerGrid(gameboardObject) {
-  const playerCells = playerGrid.querySelectorAll('.cell');
-  const grid = gameboardObject.getGrid();
-  let x = null;
-  let y = null;
-
-  grid.forEach((row, index) => {
-    y = index;
-    row.forEach((cell, index) => {
-      x = index
-      if (cell.has().ship) playerGrid.querySelector(`.x${x}.y${y}`).classList.add("has-ship");
+export function refreshGrids(gameObject) {
+  refreshGrid(gameObject.player1.gameboard, elements.playerGrid);
+  refreshGrid(gameObject.player2.gameboard, elements.opponentGrid);
+  function refreshGrid(player, gridElement) {
+    const grid = player.getGrid();
+    let x = null;
+    let y = null;
+    grid.forEach((row, index) => {
+      y = index;
+      row.forEach((cell, index) => {
+        x = index
+        if (cell.has().ship) gridElement.querySelector(`.x${x}.y${y}`).classList.add("ship");
+      })
     })
-  })
+  }
+}
+
+function cellClick(cell, name) {
+  const x = cell.dataset.x;
+  const y = cell.dataset.y;
+  console.log(name, x, y);
 }
--- logic.js
+++ logic.js
@@ -1,22 +1,12 @@
 import { Game } from "./objects.js";
-import { refreshPlayerGrid } from "./display-controller.js";
-
-let activePlayer = true;
-const stages = [null, 'placement', 'round', 'end']
-let gameStage = stages[0];
+import { refreshGrids } from "./display-controller.js";
 
 export function startGame() {
   const name = "Player";
   const activeGame = new Game(name);
   autoPlaceShips(activeGame.player1);
-  gameStage = stages[1];
-}
-
-function startRound() {
-}
-
-function endGame() {
-
+  autoPlaceShips2(activeGame.player2);
+  refreshGrids(activeGame);
 }
 
 function autoPlaceShips(player) {
@@ -25,7 +15,12 @@ function autoPlaceShips(player) {
   player.place(1, 6, 'cruiser', true);
   player.place(4, 5, 'submarine', true);
   player.place(7, 7, 'destroyer');
-  console.log(player.gameboard.getGrid());
-  player.gameboard.prettyPrintGrid();
-  refreshPlayerGrid(player.gameboard);
+}
+
+function autoPlaceShips2(player) {
+  player.place(1, 8, 'carrier');
+  player.place(8, 3, 'battleship', true);
+  player.place(1, 3, 'cruiser', true);
+  player.place(4, 2, 'submarine', true);
+  player.place(4, 6, 'destroyer');
 }
