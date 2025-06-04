b3891afd6daf80c6de25b832114b0d0cbb946ea2
scojr <180873402+scojr@users.noreply.github.com>
2025-06-04 15:59:17 -0500

    Add onButtonClick callback function to click-handler module

--- click-handler.js
+++ click-handler.js
@@ -1,11 +1,10 @@
-import { newRound } from "./logic.js";
 const playButton = document.querySelector('button.game');
 const primaryCells = document.querySelectorAll('.cell.primary');
 const opponentCells = document.querySelectorAll('.cell.opponent');
 
-playButton.addEventListener('click', (e) => {
-  newRound();
-});
+export function onButtonClick(callback) {
+  playButton.onclick = callback;
+}
 
 export function onPrimaryCellClick(callback) {
   primaryCells.forEach((cell) => cell.onclick = clickHandler);
