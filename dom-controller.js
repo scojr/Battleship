6f3cee1bb27307736db28a20520a1c15342fe210
scojr <180873402+scojr@users.noreply.github.com>
2025-07-28 17:32:41 -0500

    Create dom-controller module, style UI

---
+++ dom-controller.js
@@ -0,0 +1,35 @@
+const playerBoards = {
+  1: document.querySelector('.gameboard.player-1'),
+  2: document.querySelector('.gameboard.player-2'),
+}
+
+function updateGameboards(player1, player2) {
+
+  insertGrid(player1, playerBoards['1']);
+  insertGrid(player2, playerBoards['2']);
+
+  function insertGrid(player, gameboardEl) {
+    const grid = document.createElement('div');
+    player.gameboard.grid.forEach((row) => {
+      const rowEl = document.createElement('div');
+      rowEl.classList.add('row', 'flex');
+      row.forEach((cell) => {
+        const cellEl = document.createElement('div');
+        cellEl.classList.add('cell');
+        if (cell) {
+          cellEl.classList.add('ship')
+          if (cell.hits > 0) cellEl.classList.add('ship hit')
+        }
+        rowEl.append(cellEl);
+      })
+      gameboardEl.append(rowEl);
+    })
+  }
+
+
+}
+
+
+
+
+export { updateGameboards };
--- game-logic.js
+++ game-logic.js
@@ -1,18 +1,25 @@
 import { Player } from "./objects.js";
+import { updateGameboards } from "./dom-controller.js";
 
 let player1;
 let player2;
-
-function startGame() {
-  player1 = new Player();
-  player2 = new Player(true);
-}
+const players = [player1, player2]
 
 startGame();
 
 player1.autoPlaceShips(0);
 player2.autoPlaceShips(1);
 printGrids();
+updateGameboards(player1, player2);
+
+function startGame() {
+  player1 = new Player();
+  player2 = new Player(true);
+}
+
+function newRound() {
+
+}
 
 function printGrids() {
   console.log('\n\nPlayer 1')
