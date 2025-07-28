99b30cc0eb50055fc2842eeeaca36b03f96b89dd
scojr <180873402+scojr@users.noreply.github.com>
2025-07-27 19:28:34 -0500

    Create Player class

--- objects.js
+++ objects.js
@@ -16,16 +16,13 @@ export class Gameboard {
   constructor() {
     this.grid = createGrid();
   }
-
   ships = [];
   missedAttacks = [];
-
   placeShip(x, y, length, rotate) {
     let ship = new Ship(length);
     this.grid[x][y] = ship;
     this.ships.push(ship);
   }
-
   recieveAttack(x, y) {
     const ship = this.grid[x][y];
     if (ship) ship.hit();
@@ -34,14 +31,12 @@ export class Gameboard {
       return false
     };
   }
-
   shipsSunk() {
     for (let ship of this.ships) {
       if (!ship.isSunk()) return false;
     }
     return true;
   }
-
   getCell(x, y) {
     return this.grid[x][y];
   }
@@ -58,11 +53,11 @@ function createGrid() {
     grid.push(row);
   }
   return grid;
-
 }
 
 class Player {
-  constructor() {
-
+  constructor(isCPU = false) {
+    this.isCPU = isCPU;
   }
+  gameboard = new Gameboard;
 }
