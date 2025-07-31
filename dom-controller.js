2441009d99f669cb4a1ec960decafaf18c93a483
scojr <180873402+scojr@users.noreply.github.com>
2025-07-31 16:02:13 -0500

    Improve styling of unfocused gameboards

--- dom-controller.js
+++ dom-controller.js
@@ -53,8 +53,6 @@ export function updateGameboards(player1, player2) {
   }
 }
 
-
-
 export function gameboardDisplayControl(bool, player) {
   let elToHide = gameboardsEl;
   if (player === 1) elToHide = playerBoards[1];
@@ -62,5 +60,3 @@ export function gameboardDisplayControl(bool, player) {
   if (bool) elToHide.classList.remove('hidden');
   else elToHide.classList.add('hidden');
 }
-
-gameboardDisplayControl(false)
